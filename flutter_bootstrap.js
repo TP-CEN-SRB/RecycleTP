80cc5afc6917dbed9bcf680001b67c3f9662ac99
ongchen-crypto <2301208A@student.tp.edu.sg>
2026-01-09 12:19:28 +0800

    "Back" button final

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "585141791"
+    serviceWorkerVersion: "1776281062"
   }
 });
