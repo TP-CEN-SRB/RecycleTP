94695f5796bed3e2412a6003d5ecf350e9c751cd
ongchen-crypto <2301208A@student.tp.edu.sg>
2026-01-16 00:31:44 +0800

    New FaultReport feature

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2518671419"
+    serviceWorkerVersion: "1825916028"
   }
 });
