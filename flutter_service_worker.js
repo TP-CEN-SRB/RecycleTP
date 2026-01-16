47672745d06c362a153527694acea477951d576b
ongchen-crypto <2301208A@student.tp.edu.sg>
2026-01-16 19:33:01 +0800

    SupaBase Revert

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -65,14 +65,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "951352284705632b6a494e1a67f35565
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "b2f29acc326193c955741f71ba14e69f",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "0c7861f8962e0dc684b52f2496133fc3",
+"flutter_bootstrap.js": "9ab52f2cb60edeec59091e1e137c6d53",
 "icons/Icon-192.png": "d0e0f213fbc1f453e6f65bec12e7f7fa",
 "icons/Icon-512.png": "eb6c2c607fd875b12487cea2049e9d4f",
 "icons/Icon-maskable-192.png": "d0e0f213fbc1f453e6f65bec12e7f7fa",
 "icons/Icon-maskable-512.png": "eb6c2c607fd875b12487cea2049e9d4f",
 "index.html": "b421e37be50be2ac03b2c26ba5920a86",
 "/": "b421e37be50be2ac03b2c26ba5920a86",
-"main.dart.js": "09ecc12dc07b8c6fe93a1d4f0be6902d",
+"main.dart.js": "98f089683e1b8c98278e1e171ecd74a0",
 "manifest.json": "abda5ff7579897887be0363e276665f1",
 "version.json": "9d877a508a8889c177538f6d3c67e673"};
 // The application shell files that are downloaded before a service worker can
